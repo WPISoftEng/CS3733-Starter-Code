ef4b3b9794200eacd7f0346a6e4ee6a25ee6d1a2
Tim Winters <twinters@wpi.edu>
2019-07-19 23:28:59 -0700

    Remove main method from App

--- src/main/java/edu/wpi/teamname/App.java
+++ src/main/java/edu/wpi/teamname/App.java
@@ -5,10 +5,6 @@ import javafx.stage.Stage;
 
 public class App extends Application {
 
-  public static void main(String[] args) {
-    launch(args);
-  }
-
   @Override
   public void start(Stage primaryStage) throws Exception {
     primaryStage.show();
--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    App.main(args);
+    App.launch(App.class, args);
   }
 
 }

415a65400db7f5cef33cc1177a86e4cfb5b563b3
Tim Winters <twinters@wpi.edu>
2020-03-26 12:28:11 -0400

    Add Tim name

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -2,7 +2,7 @@ package edu.wpi.teamname;
 
 public class Main {
 
-  public static void main(String[] args) {
-    App.launch(App.class, args);
-  }
+    public static void main(String[] args) {
+        System.out.println("Tim Winters");
+    }
 }

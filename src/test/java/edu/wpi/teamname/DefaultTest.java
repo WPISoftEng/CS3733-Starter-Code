a5e4cc9da2d4c0dc10cb3a2df9515b99d558324d
Tim Winters <twinters@wpi.edu>
2020-04-07 21:45:02 -0400

    Change MainTest to DefaultTest and always pass

---
+++ src/test/java/edu/wpi/teamname/DefaultTest.java
@@ -0,0 +1,12 @@
+/*-------------------------*/
+/* DO NOT DELETE THIS TEST */
+/*-------------------------*/
+package edu.wpi.teamname;
+
+import org.junit.jupiter.api.Test;
+
+public class DefaultTest {
+
+  @Test
+  public void alwaysPass() {}
+}

7886db632f42dec5ecd736de99dff00526201eef
Tim Winters <twinters@wpi.edu>
2020-04-10 22:20:10 -0400

    Use singleton for Connection

--- src/main/java/edu/wpi/teamname/modules/DatabaseServiceProvider.java
+++ src/main/java/edu/wpi/teamname/modules/DatabaseServiceProvider.java
@@ -2,25 +2,32 @@ package edu.wpi.teamname.modules;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Singleton;
 import edu.wpi.teamname.services.DatabaseService;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * This is a Guice Module config class. This module forces guice to provide an instance of the
+ * DatabaseService as a singleton.
+ */
 public class DatabaseServiceProvider extends AbstractModule {
 
   private final String realDbUrl = "jdbc:derby:myDB;create=true";
-  /*
-   This is a Guice Module config class. This module forces
-   guice to provide an instance of the DatabaseService as a
-   singleton.
-  */
+
   @Override
   protected void configure() {
     bind(DatabaseService.class).asEagerSingleton();
   }
 
+  /**
+   * Provide single connection for database access.
+   *
+   * @throws SQLException if connection cannot be made
+   */
   @Provides
+  @Singleton
   public Connection provideConnection() throws SQLException {
     return DriverManager.getConnection(realDbUrl);
   }
